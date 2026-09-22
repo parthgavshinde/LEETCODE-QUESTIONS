f68ede8f175d498829001669c56ab8d426c6ef18
Parth Gavshinde <153381917+parthgavshinde@users.noreply.github.com>
2026-09-22 23:22:55 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (78.22%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,46 +1,20 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int low = 0, high = m * n - 1;
 
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            // Map 1D index 'mid' back to 2D coordinates
+            int val = matrix[mid / n][mid % n];
 
-        
-        
-        int r = 0 ; 
-        int c = 0 ; 
-        int r_end = matrix.size()-1;
-        int c_end= matrix[0].size()-1;
-
-        while(r<=r_end && c<=c_end)
-        {
-            int mid_r = r+ (r_end-r)/2;
-            int mid_c = c+(c_end-c)/2;
-
-            if(matrix[mid_r][mid_c]==target)
-            {
-                return true;
-            }
-            else if(target<=matrix[mid_r].back() && target>=matrix[mid_r].front() )
-            {
-                if(matrix[mid_r][mid_c]<target)
-                {
-                    c = mid_c+1;
-                }
-                else
-                {
-                    c_end = mid_c-1;
-                }
-            }
-            else if(target<matrix[mid_r].back())
-            {
-                r_end = mid_r-1;
-            }
-            else
-            {
-                r = mid_r+1;
-            }
-
+            if (val == target) return true;
+            if (val < target) low = mid + 1;
+            else high = mid - 1;
         }
-        
+
         return false;
     }
 };
