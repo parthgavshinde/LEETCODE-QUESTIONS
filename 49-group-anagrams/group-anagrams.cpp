c4ce9ae01eb0d6edc11a5b8b02912ccba99c7d29
Parth Gavshinde <153381917+parthgavshinde@users.noreply.github.com>
2026-09-22 19:18:50 +0530

    Time: 82 ms (5.02%) | Memory: 34.2 MB (10.95%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& nums) {
-        vector<vector<string>> ans;
-        unordered_map<string, int> mp;
-
-
-        int j = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            string a = nums[i];
-            sort(a.begin(),a.end());
-            if (mp.find(a) != mp.end()) {
-                ans[mp[a]].push_back(nums[i]);
-            } else {
-                mp[a] = j;
-                j++;
-                ans.push_back({nums[i]});
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        
+        for (const string& s : strs) {
+            // 26 lowercase English letters ke liye frequency array
+            vector<int> count(26, 0);
+            for (char c : s) {
+                count[c - 'a']++;
             }
+            
+            // Frequency array se unique key string banana
+            string key = "";
+            for (int i = 0; i < 26; i++) {
+                key += "#" + to_string(count[i]); // Hash `#` separator ke liye
+            }
+            
+            mp[key].push_back(s);
         }
-
+        
+        vector<vector<string>> ans;
+        for (auto& pair : mp) {
+            ans.push_back(pair.second);
+        }
+        
         return ans;
     }
 };
