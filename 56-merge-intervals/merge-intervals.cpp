416068e92272b098da95746a446b7c07b226c9dc
Parth Gavshinde <153381917+parthgavshinde@users.noreply.github.com>
2026-09-23 14:31:57 +0530

    Time: 3 ms (89.31%) | Memory: 24 MB (33.63%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,30 +1,29 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& nums) {
+        if (nums.empty()) return {};
+
+        // 1. Sort intervals based on starting points
+        sort(nums.begin(), nums.end());
 
         vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        ans.push_back({nums[0]});
+        ans.push_back(nums[0]);
 
-        int i = 1 ; 
-        int j = 0 ; 
-
-        int k = 0 ; 
-        int l = 1 ;
-        while(i<nums.size())
-        {
-            if(nums[i][j]>ans[k][l])
-            {
-                ans.push_back({nums[i]});
-                k++;
+        // 2. Iterate and merge overlapping intervals
+        for (int i = 1; i < nums.size(); ++i) {
+            // Check if current interval overlaps with the last interval in ans
+            if (nums[i][0] <= ans.back()[1]) {
+                ans.back()[1] = max(ans.back()[1], nums[i][1]);
+            } else {
+                ans.push_back(nums[i]);
             }
-            else if(nums[i][j+1]>ans[k][l])
-            {
-                ans[k][l] = nums[i][j+1];      
-            }
-            i++;
         }
+
         return ans;
     }
-
 };
