2065296e26b22bf53b6e59d2dc25c3e6d47625eb
Parth Gavshinde <153381917+parthgavshinde@users.noreply.github.com>
2026-09-24 13:00:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (66.87%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char& ch : s) {
+            if (st.empty() || ch == '{' || ch == '[' || ch == '(') {
+                st.push(ch);
+                continue;
+            }
+            if (ch == '}') {
+                if (st.top() == '{')
+                    st.pop();
+                else
+                    return false;
+            }
+            else if (ch == ']') {
+                if (st.top() == '[')
+                    st.pop();
+                else
+                    return false;
+            }
+            else{
+                if (st.top() == '(')
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        if(st.empty()) return true ;
+        return false;
+    }
+};
